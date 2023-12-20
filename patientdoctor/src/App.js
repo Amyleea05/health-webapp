2c8985fe15ba4562a7caffeb9b359eac9aa6c80a
Lovenson2000 <beaucicotlovenson@gmail.com>
2023-12-20 20:24:41 +0800

    Trying BrowserRouter

--- patientdoctor/src/App.js
+++ patientdoctor/src/App.js
@@ -1,3 +1,9 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import About from './pages/about/About';
+import Apointments from './pages/apointments/Apointments';
+import Doctors from './pages/doctors/Doctos';
+import Home from './pages/home/Home';
 import { doctors } from './utils/data';
 
 function App() {
@@ -5,7 +11,16 @@ function App() {
 
   return (
     <div className="app-container">
-      <h1>Doctor Appointment Website</h1>
+      
+      <BrowserRouter>
+        <h1>Doctor Appointment Website</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/doctors" element={<Doctors />} />
+          <Route path="/apointments" element={<Apointments />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- patientdoctor/src/pages/apointments/Apointments.jsx
+++ patientdoctor/src/pages/apointments/Apointments.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import "./about.scss";
+import "./apointments.scss";
 
 function Apointments() {
   return (
